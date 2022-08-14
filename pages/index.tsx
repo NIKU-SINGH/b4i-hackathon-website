281417b283a097a143718d0c60293225159681a1
Sandipan Dey <hey@sandipan.dev>
2022-08-14 14:11:16 +0530

    feat: preload banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { AnimatePresence, motion } from 'framer-motion'
 
 import Splash from '../components/splash'
 import HomeHOC from '../components/home'
+import Head from 'next/head'
 
 const Home: NextPage = () => {
   const [showSplash, setShowSplash] = useState(true)
@@ -13,29 +14,34 @@ const Home: NextPage = () => {
   }, [])
 
   return (
-    <div className="bg-black h-screen">
-      <AnimatePresence>
-        {showSplash ? 
-          <motion.div
-            className='absolute'
-            key="splash"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-          >
-            <Splash />
-          </motion.div> : 
-          <motion.div
-            className='absolute'
-            key="home"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-          >
-            <HomeHOC />
-          </motion.div>}
-      </AnimatePresence>
-    </div>
+    <>
+      <Head>
+        <link rel="preload" as="image" href="/btc-banner.webp" />
+      </Head>
+      <div className="bg-black h-screen">
+        <AnimatePresence>
+          {showSplash ? 
+            <motion.div
+              className='absolute'
+              key="splash"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <Splash />
+            </motion.div> : 
+            <motion.div
+              className='absolute'
+              key="home"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <HomeHOC />
+            </motion.div>}
+        </AnimatePresence>
+      </div>
+    </>
   )
 }
 
